0d7b2afef816f943c64b58460aeb60de3e4c56ad
49905784+hk4170@users.noreply.github.com <49905784+hk4170@users.noreply.github.com>
2025-10-02 12:02:06 +0000

    add proxy

--- ps.go
+++ ps.go
@@ -7,6 +7,13 @@ import (
 	"github.com/mitchellh/go-ps"
 )
 
+type proce struct{
+    pid int 
+	exec string
+}
+
+var process []proce
+
 func main() {
 	processes, err := ps.Processes()
 	if err != nil {
@@ -14,6 +21,18 @@ func main() {
 	}
 
 	for _, p := range processes {
-		fmt.Printf("PID: %d, Name: %s, PPID: %d\n", p.Pid(), p.Executable(), p.PPid())
+		//fmt.Println(p)
+		var ps proce
+		ps.pid = p.Pid()
+		ps.exec = p.Executable()
+		process = append(process, ps)
+
+		//fmt.Printf("PID: %d, Name: %s, PPID: %d\n", p.Pid(), p.Executable(), p.PPid())
+	}
+	for _,ps := range process{
+		if ps.exec == "go"{
+			fmt.Println(ps.pid)
+		}
+		
 	}
 }
